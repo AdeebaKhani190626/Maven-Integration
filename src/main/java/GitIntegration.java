55549e8abdfd8231fc059ce4a71655fe946c1b73
Adeeba Khan <i190626@nu.edu.pk>
2021-09-24 01:01:14 +0500

    This is my Initial Commit.

---
+++ src/main/java/GitIntegration.java
@@ -0,0 +1,13 @@
+
+public class GitIntegration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Doing Git Integration with Eclipse\n"); // print only prints
+		System.out.println("Java here"); // while print line prints a line too
+
+
+	}
+
+}
